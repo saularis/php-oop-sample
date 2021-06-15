7a7a14be0bd4a1a3a330e7c78389332170af53dd
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-16 02:47:23 +0600

    clean code with phpunit dataprovider

--- test/GeometryTest.php
+++ test/GeometryTest.php
@@ -10,46 +10,41 @@ use PHPUnit\Framework\TestCase;
 
 class GeometryTest extends TestCase
 {
-    /** @test */
-    public function it_can_calculate_area_of_rectangle()
+    /**
+     * @test
+     * @dataProvider geometryPerimeterProvider
+     */
+    public function it_can_calculate_geometry_perimeter($shape, $output)
     {
-        $rectangle = new Geometry(new Rectangle(10, 15));
-        $this->assertSame($rectangle->area(), 150);
+        $geometry = new Geometry($shape);
+        $this->assertSame($geometry->perimeter(), $output);
     }
 
-    /** @test */
-    public function it_can_calculate_area_of_square()
+    public function geometryPerimeterProvider()
     {
-        $square = new Geometry(new Square(10));
-        $this->assertSame($square->area(), 10*10);
+        return [
+            [new Rectangle(10, 15), (10*2) + (15*2)],
+            [new Square(10), 10*4],
+            [new Triangle(10, 20), sqrt((10 * 10) + (20 * 20)) + 20 + 10],
+        ];
     }
 
-    /** @test */
-    public function it_can_calculate_area_of_triangle()
+    /**
+     * @test
+     * @dataProvider geometryAreaProvider
+     */
+    public function it_can_calculate_geometry_area($shape, $output)
     {
-        $triangle = new Geometry(new Triangle(10, 20));
-        $this->assertSame($triangle->area(), 0.5 * 10 * 20);
+        $geometry = new Geometry($shape);
+        $this->assertSame($geometry->area(), $output);
     }
-
-    /** @test */
-    public function it_can_calculate_perimeter_of_rectangle()
+    
+    public function geometryAreaProvider()
     {
-        $rectangle = new Geometry(new Rectangle(10, 15));
-        $this->assertSame($rectangle->perimeter(), (10*2) + (15*2));
-    }
-
-    /** @test */
-    public function it_can_calculate_perimeter_of_square()
-    {
-        $square = new Geometry(new Square(10));
-        $this->assertSame($square->perimeter(), 10*4);
-    }
-
-    /** @test */
-    public function it_can_calculate_perimeter_of_triangle()
-    {
-        $triangle = new Geometry(new Triangle(10, 20));
-        $perimeter = sqrt((10 * 10) + (20 * 20)) + 20 + 10;
-        $this->assertSame($triangle->perimeter(), $perimeter);
+        return [
+            [new Rectangle(10, 15), 150],
+            [new Square(10), 10*10],
+            [new Triangle(10, 20), 0.5 * 10 * 20],
+        ];
     }
 }
